70c06b6b375397cad7df26625a0e075aeb299a0a
MacenaAndre <andremacenacosta@gmail.com>
2022-10-16 08:50:25 -0300

    /signup controller & repository

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,7 +1,12 @@
-
+import bcrypt from "bcrypt";
+import { signUpRepository } from "../repositories/auth.repositories.js";
 
 const signUp = async (req, res) => {
     const body = res.locals.Body;
+    const password = bcrypt.hashSync(body.confirmPassword, 12);
+
+    signUpRepository(body.name, body.email, password, res);
+
 };
 
 export {signUp}; 
